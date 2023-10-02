7331af1599fe605bfa1c1a72de7f1e9f0d3367b8
Mohit Joshi <withmohitjoshi@gmail.com>
2023-10-02 13:34:58 +0530

    changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -11,9 +11,23 @@ admin.initializeApp({
 expressApp.use(express.json());
 expressApp.use(
   cors({
-    origin: "http://localhost:4000",
+    origin(requestOrigin, callback) {
+      const whitelist = [
+        "http://localhost:3000",
+        "http://localhost:3001",
+        "http://localhost:3002",
+        "http://localhost:3003",        
+      ];
+      if (whitelist.indexOf(requestOrigin!) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
   })
 );
+
+expressApp.get("/", (req, res) => res.status(200).json({ message: "Welcome" }));
 
 expressApp.listen(4000, () =>
   console.log(`Server running on PORT 4000 with http://localhost:4000`)
